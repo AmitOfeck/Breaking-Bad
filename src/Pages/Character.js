5e2b5f9a650ee2d5e5fd5e719453675074553f1b
Amit Ofeck <Amitofeck1@gmail.com>
2022-02-14 17:01:03 +0200

    character comp

--- src/Pages/Character.js
+++ src/Pages/Character.js
@@ -8,18 +8,28 @@ function Character(props) {
     const params = useParams();
     const navigate = useNavigate();
 
-    const [character , setCharacter] = useState({})
+    const [character , setCharacter] = useState({
+        char_id: 0,
+        name: "",
+        birthday: "",
+        occupation: [],
+        img: "",
+        status: "",
+        nickname: "",
+        appearance: [],
+        portrayed: "",
+        category: "",
+        better_call_saul_appearance: []
+    })
 
     useEffect(async () => {
-        // console.log("hi")
         const answer = await Utils.getCharacterById(params.id)
-        // console.log(answer[0])
         setCharacter(answer[0])
     } ,[])
 
-    // let occupation = character.occupation.join(',')
-    // let appearance = character.appearance.join(',')
-    // let betterCallSaul = character.better_call_saul_appearance.join(',')
+    let occupation = character.occupation.join(',')
+    let appearance = character.appearance.join(',')
+    let betterCallSaul = character.better_call_saul_appearance.join(',')
    
     return (
         <div id="back">
@@ -29,22 +39,20 @@ function Character(props) {
             <div className="row g-0" id="white" id="detailsCard">
             <div></div>
 
-            {/* <div className="col-md-4"> */}
             <img src={character.img} className="img-fluid rounded-start" />
-            {/* </div> */}
 
             <div className="col-md-8" id="white">
             <div className="card-body">
             <h3 className="card-title">{character.name}</h3>
             <p>-------------------------------</p>
             <strong>Birthday : </strong> {character.birthday} <br/>
-            {/* <strong>Occupation : </strong>{occupation} <br/> */}
+            <strong>Occupation : </strong>{occupation} <br/>
             <strong>Status : </strong>{character.status} <br/>
             <strong>Nickname : </strong>{character.nickname} <br/>
-            {/* <strong>Appearance : </strong>{appearance} <br/> */}
+            <strong>Appearance : </strong>{appearance} <br/>
             <strong>Portrayed by : </strong>{character.portrayed} <br/>
             <strong>Category : </strong>{character.category} <br/>
-            {/* <strong>Better call saul appearance : </strong>{betterCallSaul} <br/> */}<br/><br/>
+            <strong>Better call saul appearance : </strong>{betterCallSaul} <br/><br/><br/>
             <button type="button" className="btn btn-warning" onClick={() => navigate ('/')}>Back</button>
             </div>
             </div>
